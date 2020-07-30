1aba02c0989ef23fd142fd41416bf1b3beabec84
batyr <batyrqh@protonmail.com>
2020-07-30 16:43:54 +0600

    url task, almost completed

--- src/Pages/PageListPagination/PageListPagination.js
+++ src/Pages/PageListPagination/PageListPagination.js
@@ -5,18 +5,34 @@ import { Link } from 'react-router-dom'
 import Pagination from '../../components/Pagination/Pagination'
 import PropTypes from 'prop-types'
 import qs from 'qs'
+import Button from 'react-bootstrap/Button'
 
 function PageListPagination (props) {
   const { location } = props
   const page = +qs.parse(location.search, { ignoreQueryPrefix: true }).page || 0
+  console.log(page)
 
   function handlePageClick ({ selected }) {
     const fixedSelected = selected + 1
     const { history, location } = props
 
+    // console.log('historu it is', history)
+    // console.log('location it is', location)
+
     const checkSelected = (value) => (value === 1) ? null : value
 
     const params = { page: checkSelected(fixedSelected) }
+    const updateSearch = { ...qs.parse(location.search, { ignoreQueryPrefix: true }), ...params }
+    history.push(`${location.pathname}${qs.stringify(updateSearch, { addQueryPrefix: true, skipNulls: true })}`)
+  }
+
+  function handleButtonClick () {
+    const { history, location } = props
+    // const id = +qs.parse(location.search, { ignoreQueryPrefix: true }).id
+    console.log(history)
+
+    const params = { id: 1 }
+
     const updateSearch = { ...qs.parse(location.search, { ignoreQueryPrefix: true }), ...params }
     history.push(`${location.pathname}${qs.stringify(updateSearch, { addQueryPrefix: true, skipNulls: true })}`)
   }
@@ -51,6 +67,7 @@ function PageListPagination (props) {
                 pageCount={totalPages}
                 handlePageClick={handlePageClick}
               />
+              <Button onClick={handleButtonClick}>Добавить id=1</Button>
             </>
           )
         }}
